d76baa082b0c727ac888363271380f1acb67e297
Rafa Miranda <rafael.miranda@seera.sa>
2021-04-17 13:55:30 +0200

    Create navmenu component styles, and apply responsive tweet button

--- src/Navmenu/Navmenu.js
+++ src/Navmenu/Navmenu.js
@@ -1,11 +1,16 @@
 import './Navmenu.css';
 import Navitem from '../Navitem/Navitem';
+import useWindowDimensions from '../hooks/windowDimensions'
 import navmenu from '../statics/navmenu';
 
 import TwitterIcon from '@material-ui/icons/Twitter';
+import BrushOutlinedIcon from '@material-ui/icons/BrushOutlined';
+
 import {Button} from '@material-ui/core';
 
 const Navmenu = () => {
+  const { width } = useWindowDimensions();
+
   return (
     <div className="navmenu">
       <div className="navmenu__header">
@@ -15,7 +20,9 @@ const Navmenu = () => {
         return <Navitem active={item.active} text={item.text} Icon={item.icon} key={item.text}/>
       })}
       <div className="navmenu__button">
-        <Button variant='outlined' className='navmenu__tweet'>Tweet</Button>
+        <Button variant='outlined' className='navmenu__tweet'>
+          {width < 1000 ? <BrushOutlinedIcon /> : 'Tweet'}
+        </Button>
       </div>
     </div>
   )
