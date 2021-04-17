dbdcf2ea2d6521c171cd3c8997721800f2509aab
Rafa Miranda <rafael.miranda@seera.sa>
2021-04-17 14:10:38 +0200

    Create dark-theme and its switcher feature

--- src/Navmenu/Navmenu.js
+++ src/Navmenu/Navmenu.js
@@ -5,16 +5,26 @@ import navmenu from '../statics/navmenu';
 
 import TwitterIcon from '@material-ui/icons/Twitter';
 import BrushOutlinedIcon from '@material-ui/icons/BrushOutlined';
-
-import {Button} from '@material-ui/core';
+import WbSunny from '@material-ui/icons/WbSunny';
+import Brightness2 from '@material-ui/icons/Brightness2';
+import {Button, Switch} from '@material-ui/core';
 
 const Navmenu = () => {
   const { width } = useWindowDimensions();
+
+  const switchTheme = (event) => {
+    event.target.checked ? document.querySelector('body').classList.add('dark-theme') : document.querySelector('body').classList.remove('dark-theme')
+  }
 
   return (
     <div className="navmenu">
       <div className="navmenu__header">
         <TwitterIcon className="navmenu__twitterIcon"/>
+        <span className="navmenu__theme-switcher">
+          <WbSunny/>
+          <Switch size="small" color="primary" onChange={switchTheme}/>
+          <Brightness2/>
+        </span>
       </div>
       {navmenu.map(item => {
         return <Navitem active={item.active} text={item.text} Icon={item.icon} key={item.text}/>
