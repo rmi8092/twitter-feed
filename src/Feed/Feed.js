aa641ce0ca36981f796233991acbae21ba3465a2
Rafa Miranda <rafael.miranda@seera.sa>
2021-04-18 13:10:52 +0200

    Connect app with firebase db, and fetch real dynamic data with props, useState and useEffect

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,15 +1,36 @@
-import './Feed.css'
+import {useState, useEffect} from 'react';
+
+import './Feed.css';
 import TweetForm from '../TweetForm/TweetForm';
 import Tweet from '../Tweet/Tweet';
 
+import db from './../firebase';
+
 const Feed = () => {
+  const [tweets, setTweets] = useState([]);
+  useEffect(() => {
+    db.collection('tweets').onSnapshot(snapshot => (
+      setTweets(snapshot.docs.map(document => document.data()))
+    ))
+  }, [])
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
       <TweetForm/>
-      <Tweet/>
+      {tweets.map(tweet => {
+        return (
+          <Tweet
+            avatar={tweet.avatar}
+            name={tweet.name}
+            username={tweet.username}
+            verified={tweet.verified}
+            text={tweet.text}
+            image={tweet.image}
+          />
+        )
+      })}
     </div>
   )
 }
--- src/Tweet/Tweet.js
+++ src/Tweet/Tweet.js
@@ -6,27 +6,28 @@ import RepeatIcon from '@material-ui/icons/Repeat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import PublishIcon from '@material-ui/icons/Publish';
 
-const Tweet = ({name, username, verified, text, image, avatar}) => {
+const Tweet = ({avatar, name, username, verified, text, image}) => {
   return (
     <div className="tweet">
       <div className="tweet__avatar">
-        <Avatar src="https://i.pravatar.cc/150?img=3"></Avatar>
+        <Avatar src={avatar}></Avatar>
       </div>
       <div className="tweet__body">
         <div className="tweet__header">
           <div className="tweet__header-text">
-            <h3>Rafa Miranda {' '}
+            <h3>
+              {name}
               <span className="tweet__header-special">
-                <VerifiedUserIcon className="tweet__badge"/>
-                <span>@Raf_MirIb</span>
+                {verified && <VerifiedUserIcon className="tweet__badge"/>}
+                <span>{username}</span>
               </span>
             </h3>
           </div>
           <div className="tweet__header-description">
-            <p>lorem ipsum dolor sit amet, consect</p>
+            <p>{text}</p>
           </div>
         </div>
-        <img src="https://media.giphy.com/media/LpkhKVwp6dpCg/giphy.gif" alt="" />
+        <img src={image} alt="" />
         <div className="tweet__footer">
           <ChatBubbleOutlineIcon fontSize="small"/>
           <RepeatIcon fontSize="small"/>
